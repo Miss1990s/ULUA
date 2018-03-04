5a66155b9ea2ce946655806701422a330195d51c
Hesinian <sinianhe@foxmail.com>
2018-03-04 20:38:24 +0800

    first

---
+++ ConsoleApplication1/ConsoleApplication1/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuaInterface;
+
+namespace ConsoleApplication1
+{
+    class Helper
+    {
+        /// <summary>
+        /// 遍历打印栈
+        /// </summary>
+        /// <param name="L"></param>
+        public static void stackDump(IntPtr L)
+        {
+            int i;
+            int top = LuaDLL.lua_gettop(L);
+            for (i = 1; i < top; i++)
+            {
+                LuaTypes t = LuaDLL.lua_type(L, i);
+                switch (t)
+                {
+                    case LuaTypes.LUA_TSTRING:
+                        {
+                            Console.WriteLine("'%s'", LuaDLL.lua_tostring(L, i));
+                            break;
+                        }
+                    case LuaTypes.LUA_TBOOLEAN:
+                        {
+                            Console.WriteLine("'%s'", LuaDLL.lua_toboolean(L, i));
+                            break;
+                        }
+                    case LuaTypes.LUA_TNUMBER:
+                        {
+                            Console.WriteLine("'%s'", LuaDLL.lua_tonumber(L, i));
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("'%s'", LuaDLL.luaL_typename(L, t));
+                            break;
+                        }
+                }
+                Console.WriteLine("    ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuaInterface;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IntPtr L = LuaDLL.luaL_newstate();
+            LuaDLL.luaL_openlibs(L);
+            string buff;
+            int error;
+            while ( (buff = Console.ReadLine()) != null){
+                error = LuaDLL.luaL_loadstring(L, buff) + LuaDLL.lua_pcall(L, 0, 0, 0);
+                if (error !=0)
+                {
+                    Console.WriteLine("error " + LuaDLL.lua_tostring(L, -1));
+                    LuaDLL.lua_pop(L, 1);
+                }
+            }
+            LuaDLL.lua_close(L);
+            return;
+        }
+    }
+}
